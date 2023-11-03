1a5a1c83c65d04f8f3b3210a9c9cd9d8061b1b82
Tomáš Witek <tomas@witek.cz>
2023-11-03 10:57:00 +0100

    Update launch test

--- packages/admin-extensions/endpoints/endpoint-test/src/index.ts
+++ packages/admin-extensions/endpoints/endpoint-test/src/index.ts
@@ -2,6 +2,7 @@ import {defineEndpoint} from "@directus/extensions-sdk";
 
 export default defineEndpoint((router) => {
   console.log("Test endpoint loaded");
+  console.log("Test endpoint loadedd");
   router.get("/", (_req, res) => {
     console.log("Ahoj");
     res.send("Hello, World!");
