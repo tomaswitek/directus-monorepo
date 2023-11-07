6c56450d16b8e2210f18a0532ddeaee1be29d534
Tomáš Witek <tomas@witek.cz>
2023-11-07 16:05:21 +0100

    Change id of the endpoint

--- packages/directus-extensions/endpoints/directus-extension-endpoint-example/src/index.ts
+++ packages/directus-extensions/endpoints/directus-extension-endpoint-example/src/index.ts
@@ -1,9 +1,12 @@
 import {defineEndpoint} from "@directus/extensions-sdk";
 
-export default defineEndpoint((router) => {
-  console.log("Test endpoint loaded");
-  router.get("/", (_req, res) => {
-    console.log("Ahoj");
-    res.send("Hello, World!");
-  });
+export default defineEndpoint({
+  id: "test-endpoint",
+  handler: (router) => {
+    console.log("Test endpoint loaded");
+    router.get("/", (_req, res) => {
+      console.log("Ahoj");
+      res.send("Hello, World!");
+    });
+  },
 });
